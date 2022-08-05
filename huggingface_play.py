3dfe99225e3e51fd1b80b988f53bbbd6ca387c25
tomaszodrzygozdz <tomeko314@gmail.com>
2022-08-05 23:34:33 +0200

    Huggingface basic training works

--- huggingface_play.py
+++ huggingface_play.py
@@ -1,5 +1,8 @@
 import torch
-from transformers import BartTokenizer, BartForConditionalGeneration, BartConfig
+
+torch.cuda.empty_cache()
+
+from transformers import BartTokenizer, BartForConditionalGeneration, BartConfig, TrainingArguments, Trainer
 
 from datasets import load_dataset
 
@@ -15,25 +18,49 @@ class ChesHFDataset(torch.utils.data.Dataset):
 
     def __getitem__(self, idx):
         # item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
-        item = {'input_ids' : [0,1,0,1], 'attention_maks': [1,1,1,1], 'labels': [1,0,1,0]}
+        item = {'input_ids' : [0,1,0,1], 'attention_maks': [1,1,1,1], 'labels': [2,2,2,2]}
         # item['labels'] = torch.tensor(self.labels[idx])
         return item
 
     def __len__(self):
-        return 100
+        return 1000
 #
 #
-# model = BartForConditionalGeneration(BartConfig())
 
-train_texts = ["elo", "melo", "Morelo"]
+train_dataset = ChesHFDataset()
+val_dataset = ChesHFDataset()
+test_dataset = ChesHFDataset()
 
-tokenizer = BartTokenizer.from_pretrained("facebook/bart-base")
+model = BartForConditionalGeneration(BartConfig(vocab_size=5, max_position_embeddings=16, encoder_ffn_dim=512, decoder_ffn_dim=512))
+# model = BartForConditionalGeneration(BartConfig(vocab_size=5, max_position_embeddings=16, encoder_layers=4, decoder_layers=4, encoder_ffn_dim=128, decoder_ffn_dim=128))
 
-encodings = tokenizer(train_texts, truncation=True, padding=True)
+training_args = TrainingArguments(
+    output_dir='./results',          # output directory
+    num_train_epochs=3,              # total number of training epochs
+    per_device_train_batch_size=2,  # batch size per device during training
+    per_device_eval_batch_size=2,   # batch size for evaluation
+    warmup_steps=500,                # number of warmup steps for learning rate scheduler
+    weight_decay=0.01,               # strength of weight decay
+    logging_dir='./logs',            # directory for storing logs
+    logging_steps=10,
+)
 
-input = tokenizer("Ufo ufo wie lepiej", return_tensors="pt").input_ids
+trainer = Trainer(
+    model=model,                         # the instantiated 🤗 Transformers model to be trained
+    args=training_args,                  # training arguments, defined above
+    train_dataset=train_dataset,         # training dataset
+    eval_dataset=val_dataset             # evaluation dataset
+)
 
-uu = torch.IntTensor([[10,20,30]])
+trainer.train()
+
+# tokenizer = BartTokenizer.from_pretrained("facebook/bart-base")
+
+# encodings = tokenizer(train_texts, truncation=True, padding=True)
+
+# input = tokenizer("Ufo ufo wie lepiej", return_tensors="pt").input_ids
+
+# uu = torch.IntTensor([[10,20,30]])
 
 # outputs = model.generate(torch.IntTensor([[10,20,30]]), num_beams=4, num_return_sequences=2)
 
