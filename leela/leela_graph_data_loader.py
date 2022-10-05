cb102daae4a49c6aa119929c698d73a7c2bf9ec4
tomaszodrzygozdz <tomeko314@gmail.com>
2022-10-05 14:23:12 +0200

    LeelaGMLTree iterator

--- leela/leela_graph_data_loader.py
+++ leela/leela_graph_data_loader.py
@@ -1,4 +1,4 @@
-from typing import List, ByteString, Tuple, Dict
+from typing import List, ByteString, Tuple, Dict, Iterable
 
 import chess
 import matplotlib.pyplot as plt
@@ -25,32 +25,21 @@ def current_position(input_board: str, moves: List[str]) -> str:
 
 
 class LeelaGMLTree:
-    def __init__(self, path_to_data: str, input_board: str, create_all_states: bool = False):
-        self.path_to_data = path_to_data
+    def __init__(self, lines, input_board: str, create_all_states: bool = False):
+        self.lines = lines
         self.input_board = input_board
         self.create_all_states = create_all_states
 
         self.graph = self.to_graph()
 
-    def data_trees_generator(self) -> Tuple[str, List[ByteString]]:
-        with open(self.path_to_data, "rb") as dataset:
-            while True:
-                graph: List[ByteString] = []
-                input_board: str = dataset.readline().decode("UTF-8")[:-1]
-                for line in dataset:
-                    if line == b"\n":
-                        break
-                    graph.append(line)
-                yield input_board, graph
-
     def to_graph(self) -> nx.classes.digraph.DiGraph:
-        G: nx.classes.digraph.DiGraph = nx.readwrite.gml.read_gml(self.path_to_data, label="id")
+        G: nx.classes.digraph.DiGraph = nx.readwrite.gml.parse_gml(self.lines, label="id")
         if self.create_all_states:
             for node in G.nodes:
                 self.create_state(node)
         return G
 
-    def create_state(self, node):
+    def create_state(self, node) -> str:
         if "state" not in self.graph.nodes[node]:
             return self.graph.nodes[node]["state"]
         else:
@@ -73,7 +62,7 @@ class LeelaGMLTree:
         all_k_successors = list(nx.dfs_successors(self.graph, node, depth_limit=k + 1))
         for node in all_k_successors:
             self.create_state(node)
-        k_successors_dist = {
+        k_successors_data = {
             succ_node: {
                 "dist": self.distance_to_predecessors(succ_node, node),
                 "state": self.graph.nodes[succ_node]["state"],
@@ -81,16 +70,16 @@ class LeelaGMLTree:
                 "Q": self.graph.nodes[succ_node]["Q"],
                 "D": self.graph.nodes[succ_node]["D"],
                 "M": self.graph.nodes[succ_node]["M"],
-                "P": self.graph.nodes[succ_node]["P"]
+                "P": self.graph.nodes[succ_node]["P"],
             }
             for succ_node in all_k_successors
         }
-        return k_successors_dist
+        return k_successors_data
 
     def get_parent(self, node):
         return list(self.graph.predecessors(node))[0]
 
-    def distance_to_predecessors(self, node, predecessor):
+    def distance_to_predecessors(self, node, predecessor) -> int:
         dist = 0
         if node != predecessor:
             parent = self.get_parent(node)
@@ -99,3 +88,17 @@ class LeelaGMLTree:
                 parent = self.get_parent(parent)
                 dist += 1
         return dist
+
+
+def data_trees_generator(data_path: str, create_all_states: bool = False) -> Iterable[LeelaGMLTree]:
+    with open(data_path, "rb") as dataset:
+        while True:
+            graph_lines: List[str] = []
+            input_board: str = dataset.readline().decode("UTF-8")[:-1]
+
+            for line in dataset:
+                line = line.decode("ascii")
+                if line == "\n":
+                    break
+                graph_lines.append(line)
+            yield LeelaGMLTree(graph_lines, input_board, create_all_states)
