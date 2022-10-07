3ef2c4f60e5de0f13722a5a47972ab9ef2848999
tomaszodrzygozdz <tomeko314@gmail.com>
2022-10-08 00:40:44 +0200

    BFS nodes selector working

--- data_processing/mcts_data_generator.py
+++ data_processing/mcts_data_generator.py
@@ -43,38 +43,40 @@ class HighestNSelector(LeelaTreeTargetSelector):
 
 
 class BFSNodeSelector:
-    def __init__(self, root: int, graph: LeelaGMLTree, k: int, n_subgoals: int):
-        self.root = root
-        self.graph = graph
-        self.k = k
-        self.n_subgoals = n_subgoals
+    def find_subgoals(
+        self, root: int, graph: LeelaGMLTree, k: int, n_subgoals: int, max_iters: int = 200
+    ) -> List[LeelaSubgoal]:
 
-        self.selector = HighestNSelector()
-
-    def find_subgoals(self) -> List[LeelaSubgoal]:
-        self.queue = [self.root]
-        self.visited = set()
+        selector = HighestNSelector()
+        queue = [root]
+        visited = set()
         subgoals = []
+        iters = 0
 
-        while len(self.queue) > 0:
-            node = self.queue.pop(0)
-            self.visited.add(node)
-            subgoals = self.selector.select_subgoals(node, self.graph, self.k, self.n_subgoals)
-            subgoals.extend(subgoals)
-            for subgoal in subgoals:
-                self.queue.append(subgoal.target_idx)
+        while len(queue) > 0 and iters < max_iters:
+            iters += 1
+            node = queue.pop(0)
+            if node not in visited:
+                visited.add(node)
+                subgoals.extend(selector.select_subgoals(node, graph, k, n_subgoals))
+                for subgoal in subgoals:
+                    if subgoal.target_idx not in queue:
+                        queue.append(subgoal.target_idx)
 
-
+        print(f"len(subgoals) = {len(subgoals)}")
+        return subgoals
 
 
 class SubgoalMCGamesDataGenerator:
-    def __init__(self, k, n_subgoals, fraction_of_graph_to_use, log_samples_limit=50):
+    def __init__(self, k, n_subgoals, fraction_of_graph_to_use, log_samples_limit=200):
         self.k = k
         self.n_subgoals = n_subgoals
         self.fraction_of_graph_to_use = fraction_of_graph_to_use
 
-        self.input_selector = RandomLeelaTreeInputSelector()
-        self.target_selector = HighestNSelector()
+        # self.input_selector = RandomLeelaTreeInputSelector()
+        # self.target_selector = HighestNSelector()
+        self.nodes_selector = BFSNodeSelector()
+
         self.log_samples_limit = log_samples_limit
 
         self.data = {}
@@ -89,11 +91,7 @@ class SubgoalMCGamesDataGenerator:
         return ChessDataset(self.data)
 
     def leela_tree_to_datapoints(self, leela_tree: LeelaGMLTree):
-        selected_inputs = self.input_selector.select_inputs(leela_tree, self.fraction_of_graph_to_use)
-        subgoals = []
-        for input_node in selected_inputs:
-            subgoals.extend(self.target_selector.select_subgoals(input_node, leela_tree, self.k, self.n_subgoals))
-        self.processed_trees += 1
+        subgoals = self.nodes_selector.find_subgoals(0, leela_tree, self.k, self.n_subgoals)
         if self.logged_samples < self.log_samples_limit:
             self.log_samples(subgoals)
         self.log_subgoals_stats(subgoals)
@@ -105,17 +103,17 @@ class SubgoalMCGamesDataGenerator:
 
     def log_samples(self, subgoals: List[LeelaSubgoal]) -> None:
         for subgoal in subgoals:
-            if random.random() < 0.1:
+            if random.random() < 2:
                 self.log_leela_subgoal(subgoal)
 
     def log_leela_subgoal(self, leela_subgoal: LeelaSubgoal):
         self.logged_samples += 1
         img = immutable_boards_to_img(
             [
-                ImmutableBoard.from_fen_str(leela_subgoal.input_board),
-                ImmutableBoard.from_fen_str(leela_subgoal.target_board),
+                ImmutableBoard.from_fen_str(leela_subgoal.input_fen),
+                ImmutableBoard.from_fen_str(leela_subgoal.target_fen),
             ],
             [f"Input Moves={leela_subgoal.moves} Lvl={leela_subgoal.input_level}", f"Target. N = {leela_subgoal.N} "],
         )
         # img.save(f"/home/tomek/Research/subgoal_search_chess/tmp_{random.randint(0, 10**4)}.png")
-        img.savefig(f"/home/tomek/Research/subgoal_search_chess/tmp/{random.randint(0, 10 ** 4)}.png")
+        img.savefig(f"/home/tomek/Research/subgoal_search_chess/tmp/{random.randint(0, 10 ** 7)}.png")
