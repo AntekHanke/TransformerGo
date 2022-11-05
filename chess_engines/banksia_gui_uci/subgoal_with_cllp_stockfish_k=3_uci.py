5824a4d4a12efd60d51d600f10a75046c2992321
gracjangoral <gracjan.goral.b@gmail.com>
2022-11-05 15:01:27 +0100

    changings in paths

--- chess_engines/banksia_gui_uci/subgoal_with_cllp_stockfish_k=3_uci.py
+++ chess_engines/banksia_gui_uci/subgoal_with_cllp_stockfish_k=3_uci.py
@@ -7,16 +7,18 @@ set_root_path()
 from chess_engines.banksia_gui_uci.banksia_gui_core import main_uci_loop
 from chess_engines.bots.basic_chess_engines import SubgoalWithCLLPStockfish
 
-path_to_generator: str = ""
-path_to_cllp: str = ""
-log_dir: str = ""
+path_to_generator: str = "/home/gracjan/subgoal/subgoal_chess_data/generator/final_model"
+path_to_cllp: str = "/home/gracjan/subgoal/subgoal_chess_data/cllp/final_model"
+log_dir: str = "/home/gracjan/subgoal/log_exp_subgoal_chess_engines"
 
 engine = SubgoalWithCLLPStockfish(
     generator_checkpoint=path_to_generator,
     cllp_checkpoint=path_to_cllp,
     n_subgoals=3,
     stockfish_depth=20,
-    log_dir="/home/tomasz/Research/subgoal_chess_data/bot_logs",
+    log_dir=log_dir,
+    debug_mode=True,
+    replace_legall_move_with_random=False
 )
 
 if __name__ == "__main__":
