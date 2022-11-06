04dfb7b03b9a20c4d044d3af9213c954b5f7b612
gracjangoral <gracjan.goral.b@gmail.com>
2022-11-06 21:19:38 +0100

    adding path

--- chess_engines/banksia_gui_uci/random_chess_engine.py
+++ chess_engines/banksia_gui_uci/random_chess_engine.py
@@ -1,7 +1,8 @@
 #!/home/gracjan/anaconda3/envs/subgoal_chess/bin/python3 -u
-from set_root_path import set_root_path
-
-set_root_path()
+import sys
+import os
+sys.path.append("/home/gracjan/subgoal/subgoal_search_chess-uci_engines")
+os.environ["SUBGOAL_PROJECT_ROOT"] = "/home/gracjan/subgoal/subgoal_search_chess-uci_engines"
 
 from chess_engines.banksia_gui_uci.banksia_gui_core import main_uci_loop
 from chess_engines.bots.basic_chess_engines import RandomChessEngine
