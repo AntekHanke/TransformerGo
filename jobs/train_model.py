50902ceb99af439e1b413f59dcb3e04577a0c80e
TomaszOdrzygozdz <tomeko314@gmail.com>
2022-12-20 14:26:06 +0100

    Experiments fixed

--- jobs/train_model.py
+++ jobs/train_model.py
@@ -1,4 +1,4 @@
-from typing import Type, Union, List
+from typing import Type, Union, List, Optional
 from transformers.integrations import NeptuneCallback
 from data_processing.pandas_data_provider import IterableDataLoader
 
@@ -20,8 +20,8 @@ class TrainModel(Job):
     def __init__(
         self,
         iterable_dataset_class: Type[IterableDataLoader],
-        path_to_training_data: Union[str, List[str]],
-        path_to_eval_data: Union[str, List[str]],
+        path_to_training_data: Optional[str] = None,
+        path_to_eval_data: Optional[str] = None,
         files_batch_size: int = 10,
         take_random_half_of_training_data: bool = False,
         take_random_half_of_eval_data: bool = False,
@@ -30,21 +30,28 @@ class TrainModel(Job):
         output_dir: str = None,
     ) -> None:
 
-        x = GlobalParamsHandler()
+        global_params_handler: GlobalParamsHandler = GlobalParamsHandler()
+        data_paths_from_gloabl_params_handler = global_params_handler.get_data_path()
+        output_dir_from_global_params_handler = global_params_handler.get_out_dir()
 
-        if GlobalParamsHandler().get_out_dir() is not None:
-            output_dir = GlobalParamsHandler().get_out_dir()
+        self.path_to_training_data = path_to_training_data
+        self.path_to_eval_data = path_to_eval_data
+        if data_paths_from_gloabl_params_handler is not None:
+            self.path_to_training_data, self.path_to_eval_data = data_paths_from_gloabl_params_handler
+
+        self.output_dir = output_dir
+        if output_dir_from_global_params_handler is not None:
+            self.output_dir = output_dir_from_global_params_handler
 
         self.path_to_training_data = path_to_training_data
         self.path_to_eval_data = path_to_eval_data
         self.take_random_half_of_training_data = take_random_half_of_training_data
         self.take_random_half_of_eval_data = take_random_half_of_eval_data
 
-        log_param("output_dir", output_dir)
         # TODO: How to fix typing and class initialization
         self.training_args = training_args_cls(output_dir=output_dir + "/out")
-        if GlobalParamsHandler().learning_rate is not None:
-            self.training_args.learning_rate = GlobalParamsHandler().learning_rate
+        if global_params_handler.learning_rate is not None:
+            self.training_args.learning_rate = global_params_handler.learning_rate
 
         self.iterable_subgoal_dataLoader_train = iterable_dataset_class(
             data_path=self.path_to_training_data,
@@ -61,20 +68,22 @@ class TrainModel(Job):
         self.model_config = model_config_cls()
         self.model = BartForConditionalGeneration(self.model_config)
 
-        log_param("real learning rate", self.training_args.learning_rate)
-
         self.trainer = Trainer(
             model=self.model,
             args=self.training_args,
             train_dataset=self.iterable_subgoal_dataLoader_train,
-            eval_dataset=self.iterable_subgoal_dataLoader_eval,
-            # compute_metrics=compute_metrics,
+            eval_dataset=self.iterable_subgoal_dataLoader_eval
         )
 
         for callback_logger in pytorch_callback_loggers:
             self.trainer.add_callback(callback_logger.get_pytorch_callback())
         self.trainer.pop_callback(NeptuneCallback)
         self.save_model_path = output_dir + "/final_model"
+
+        log_param("max learning rate", self.training_args.learning_rate)
+        log_param("output_dir", self.output_dir)
+        log_param("path_to_training_data", self.path_to_training_data)
+        log_param("path_to_eval_data", self.path_to_eval_data)
         log_param("Save model path", self.save_model_path)
 
     def execute(self) -> None:
