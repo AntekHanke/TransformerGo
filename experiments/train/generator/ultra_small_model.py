cd8df68df541d0854b2d2095e3efc44ed767e7ae
gracjangoral <gracjan.goral.b@gmail.com>
2022-12-27 14:00:26 +0100

    changing spec

--- experiments/train/generator/ultra_small_model.py
+++ experiments/train/generator/ultra_small_model.py
@@ -2,27 +2,22 @@ from datetime import date
 
 from mrunner.helpers.specification_helper import create_experiments_helper
 
-from metric_logging import source_files_register
-
-import os
-
-source_files_register.register(os.path.dirname(os.path.abspath('__file__')))
-print(f"__file__ = {os.path.dirname(os.path.abspath('__file__'))}")
-
 
 base_config = {
     "run.job_class": "@jobs.TrainModel",
     "TrainModel.iterable_dataset_class": "@data.IterableSubgoalDataLoader",
 
-    "TrainModel.path_to_training_data": "/leela_generator_data/train/generator",
-    "TrainModel.path_to_eval_data": "/leela_generator_data/eval",
+    "TrainModel.path_to_training_data": None,
+    "TrainModel.path_to_eval_data": None,
 
     "TrainModel.model_config_cls": "@transformers.BartConfig",
     "TrainModel.training_args_cls": "@transformers.TrainingArguments",
 
-    "GlobalParamsHandler.out_dir": f"/leela_models/test_training_generator/leela_models/generator/ultra_small_model/{date.today()}",
-    "GlobalParamsHandler.path_type": "full_info",
-    "GlobalParamsHandler.path_format": ["learning_rate"],
+    "GlobalParamsHandler.path_type": "generator",
+    "GlobalParamsHandler.k": 1,
+    "GlobalParamsHandler.data_location": "/chess_data",
+    "GlobalParamsHandler.out_dir": f"/leela_models/generator/ultra_small_model/{date.today()}",
+    "GlobalParamsHandler.path_format": ["k", "learning_rate"],
 
     "BartConfig.vocab_size": 4562,
     "BartConfig.max_position_embeddings": 90,
@@ -43,18 +38,19 @@ base_config = {
     "TrainingArguments.logging_steps": 2,
     "TrainingArguments.evaluation_strategy": "steps",
     "TrainingArguments.eval_steps": 200,
-    "TrainingArguments.learning_rate": 0.0002,
+    "TrainingArguments.learning_rate": 3e-5,
 
     "use_neptune": True,
 }
 
 params_grid = {
     "idx": [0],
-    "GlobalParamsHandler.learning_rate": [0.0001],
+    "GlobalParamsHandler.k": [1],
+    "GlobalParamsHandler.learning_rate": [3e-5],
 }
 
 experiments_list = create_experiments_helper(
-    experiment_name=f"ultra-small-leela-subgoals_k=1",
+    experiment_name=f"ultra-small-leela-subgoals_k=1 - test",
     project_name="pmtest/subgoal-chess",
     base_config=base_config,
     params_grid=params_grid,
