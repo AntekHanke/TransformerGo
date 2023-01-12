5f6130786d849e0259cc53718196a2bf85c8db37
gracjangoral <gracjan.goral.b@gmail.com>
2023-01-12 14:03:15 +0100

    comment CLLPOneMove class

--- configures/gin_configurable_classes.py
+++ configures/gin_configurable_classes.py
@@ -14,7 +14,7 @@ from data_processing.pandas_data_provider import (
 )
 from data_processing.prepare_and_save_data import PandasPolicyPrepareAndSaveData, CLLPPrepareAndSaveData
 from jobs.create_pgn_dataset import CreatePGNDataset
-from jobs.evaluate_cllp import EvaluateCLLP
+# from jobs.evaluate_cllp import EvaluateCLLP
 from jobs.job_leela_dataset import LeelaCCLPDataProcessing, LeelaParallelDatasetGenerator, LeelaPrepareAndSaveData
 from jobs.train_bert_for_sequence_model import TrainBertForSequenceModel
 from jobs.train_model import TrainModel
@@ -35,7 +35,7 @@ configure_classes(
         TrainModel,
         CreatePGNDataset,
         LeelaCCLPDataProcessing,
-        EvaluateCLLP,
+        # EvaluateCLLP,
         TrainBertForSequenceModel,
         LeelaParallelDatasetGenerator,
         LeelaPrepareAndSaveData
