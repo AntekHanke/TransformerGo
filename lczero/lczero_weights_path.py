26f53a7131c955eb373649eb06ad2cf9c810187d
gracjangoral <gracjan.goral.b@gmail.com>
2023-01-25 17:28:24 +0100

    changing names

--- lczero/lczero_weights_path.py
+++ lczero/lczero_weights_path.py
@@ -6,6 +6,6 @@ if is_local_machine():
     if get_local_machine() == "tomasz":
         DEFAULT_LC_ZERO_WEIGHTS_PATH = "/lczero/medium"
     elif get_local_machine() == "gracjan":
-        DEFAULT_LC_ZERO_WEIGHTS_PATH = None
+        DEFAULT_LC_ZERO_WEIGHTS_PATH = "/home/gracjan/medium"
 else:
-    DEFAULT_LC_ZERO_WEIGHTS_PATH = "/main_storage/lczero/medium"
+    DEFAULT_LC_ZERO_WEIGHTS_PATH = "/lcezero_weights/medium"
