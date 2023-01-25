b931902d3128a3fef27e5b056bfa4bfe2c81c598
TomaszOdrzygozdz <tomeko314@gmail.com>
2023-01-25 15:53:26 +0100

    debug_job -> lczero

--- jobs/debug_job.py
+++ jobs/debug_job.py
@@ -9,11 +9,13 @@ from policy.chess_policy import LCZeroPolicy
 class DebugJob:
     """Any code goes here"""
     def execute(self):
+        import chess
+        from data_structures.data_structures import ImmutableBoard
+        from policy.chess_policy import LCZeroPolicy
+
         policy = LCZeroPolicy()
-        board = chess.Board()
-        time_start = time.time()
-        for _ in range(30):
-            moves = policy.get_best_moves(ImmutableBoard.from_board(board), 5)
-            print(moves)
-            board.push(moves[0])
-        print(time.time() - time_start)
+        b = chess.Board()
+        bb = ImmutableBoard.from_board(b)
+        y = policy.get_best_moves(bb)
+        print(y)
+
