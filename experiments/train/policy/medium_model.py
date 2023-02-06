8adc360f8235273f40ccba592ed0548bfd2000dd
Gosia <rog.malgosia@gmail.com>
2023-02-06 11:39:31 +0100

    Add medium model job configs

---
+++ experiments/train/policy/medium_model.py
@@ -0,0 +1,54 @@
+from datetime import date
+from mrunner.helpers.specification_helper import create_experiments_helper
+
+base_config = {
+    "run.job_class": "@jobs.TrainModel",
+
+    "TrainModel.iterable_dataset_class": "@data.IterablePolicyDataLoader",
+    "TrainModel.path_to_training_data": "/data/k_3",
+    "TrainModel.path_to_eval_data": "/data/k_3_eval",
+    "TrainModel.files_batch_size": 1,
+    "TrainModel.model_config_cls": "@transformers.BartConfig",
+    "TrainModel.training_args_cls": "@transformers.TrainingArguments",
+    "TrainModel.out_dir": "/out/policy/medium/no_history",
+
+    "BartConfig.vocab_size": 4600,
+    "BartConfig.max_position_embeddings": 100,
+    "BartConfig.encoder_layers": 4,
+    "BartConfig.decoder_layers": 4,
+    "BartConfig.encoder_attention_heads": 2,
+    "BartConfig.decoder_attention_heads": 2,
+    "BartConfig.decoder_ffn_dim": 128,
+    "BartConfig.encoder_ffn_dim": 128,
+    "BartConfig.d_model": 32,
+    "BartConfig.dropout": 0.05,
+
+    "TrainingArguments.max_steps": 5000,
+    "TrainingArguments.per_device_train_batch_size": 1,
+    "TrainingArguments.per_device_eval_batch_size": 1,
+    "TrainingArguments.warmup_steps": 10,
+    "TrainingArguments.weight_decay": 0.01,
+    "TrainingArguments.logging_steps": 2,
+    "TrainingArguments.evaluation_strategy": "steps",
+    "TrainingArguments.eval_steps": 20,
+    "TrainingArguments.learning_rate": 2e-4,
+
+    "use_neptune": True,
+}
+
+params_grid = {
+    "idx": [0],
+}
+
+experiments_list = create_experiments_helper(
+    experiment_name=f"medium-policy-model",
+    project_name="pmtest/subgoal-chess",
+    base_config=base_config,
+    params_grid=params_grid,
+    script="python3 -m runner --mrunner",
+    exclude=["data", ".pytest_cache", "out", ".git"],
+    python_path="",
+    tags=["policy", "train", "medium"],
+    with_neptune=True,
+    env={},
+)
