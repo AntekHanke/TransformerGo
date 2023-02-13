d66204ea9596ef256b100863ed170854e953edd6
gracjangoral <gracjan.goral.b@gmail.com>
2023-02-13 20:53:30 +0100

    Adding condition for importing backend for different users.

--- lczero/lczero_general_backend/lczero_classes.py
+++ lczero/lczero_general_backend/lczero_classes.py
@@ -2,10 +2,11 @@ from typing import Optional
 
 from configures.global_config import LCZERO_CLUSTER
 from lczero.lczero_weights_path import DEFAULT_LC_ZERO_WEIGHTS_PATH
-from utils.detect_local_machine import is_local_machine
+from utils.detect_local_machine import is_local_machine, get_local_machine
 
 # global lczero_backend
 lczero_backend = None
+
 
 class LCZeroBackend:
     def __init__(self, weights_class, backend_class, game_state_class, weights_path: Optional[str] = None):
@@ -33,22 +34,41 @@ class LCZeroBackend:
         sorted_moves = self.get_policy_distribution(immutable_board)
         return {move[0]: move[1] for move in sorted_moves}
 
+
 def get_lczero_backend(weights_path=None):
     """Generates a new LCZero backend if one doesn't exist, otherwise returns the existing one."""
     global lczero_backend
     if lczero_backend is None:
         if is_local_machine():
-            from lczero.lczero_backend_local.lczero_local import lc_zero_local_backend
-            lczero_backend = lc_zero_local_backend
+            user = get_local_machine()
+            if user == "TomaszOpc":
+                from lczero.lczero_backend_local.lczero_backend_local_tomek.lczero_local import lc_zero_local_backend
+
+                lczero_backend = lc_zero_local_backend
+            elif user == "g":
+                from lczero.lczero_backend_local.lczero_backend_local_gg.lczero_local import lc_zero_local_backend
+
+                lczero_backend = lc_zero_local_backend
+            elif user == "dell-latitude-e7450":
+                from lczero.lczero_backend_local.lczero_backend_local_malgorzata.lczero_local import (
+                    lc_zero_local_backend,
+                )
+
+                lczero_backend = lc_zero_local_backend
         else:
             if LCZERO_CLUSTER == "athena":
                 from lczero.lczero_backend_athena.lczero_athena import lc_zero_athena_backend
+
                 lczero_backend = lc_zero_athena_backend
             elif LCZERO_CLUSTER == "prometheus":
                 from lczero.lczero_backend_prometheus.lczero_prometheus import lc_zero_prometheus_backend
+
                 lczero_backend = lc_zero_prometheus_backend
             else:
-                raise Exception("Invalid LCZero cluster. Set the value of LCZERO_CLUSTER in global config to either 'athena' or 'prometheus'.")
+                raise Exception(
+                    "Invalid LCZero cluster. Set the value of LCZERO_CLUSTER in global config to either 'athena' or "
+                    "'prometheus'."
+                )
         return lczero_backend
     else:
         return lczero_backend
