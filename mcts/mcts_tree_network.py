46b8c14017fd9f5af1bb0be4dbc6a84df023df8a
Gosia <rog.malgosia@gmail.com>
2023-03-01 18:48:57 +0100

    Change labelling of nodes and edges

--- mcts/mcts_tree_network.py
+++ mcts/mcts_tree_network.py
@@ -5,7 +5,8 @@ from mcts.mcts import Tree
 def mcts_tree_network(tree: Tree, target_file: str):
     network = Network(layout=True, directed=True)
     for node in tree.node_list:
-        network.add_node(n_id=node.id, label=str(node.state.to_board()), level=node.level)
+        value = node.total_value / node.num_visits
+        network.add_node(n_id=node.id, label=f"{value:.4f}", level=node.level)
         if node.parent is not None:
-            network.add_edge(source=node.parent.id, to=node.id, weight=node.probability)
+            network.add_edge(source=node.parent.id, to=node.id, title=f"{node.probability:.4f}")
     network.save_graph(target_file)
