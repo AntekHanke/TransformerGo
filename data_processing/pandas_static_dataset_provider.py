ecfd0f2bae7fcfa899a4d7a447da7abc59f40763
gracjan <gracjan.goral.b@gmail.com>
2023-03-06 17:56:54 +0100

    adding PandasStaticSubgoalAllDistancesDataProvider, which generates subgoals for all k

--- data_processing/pandas_static_dataset_provider.py
+++ data_processing/pandas_static_dataset_provider.py
@@ -12,6 +12,7 @@ from data_processing.data_processing_functions import (
     policy_with_history_process_df,
     subgoal_to_policy_process_df,
     cllp_process_df,
+    subgoal_all_k_process_df,
 )
 from metric_logging import log_object, log_value
 
@@ -48,7 +49,9 @@ class PandasStaticDataProvider(torch.utils.data.Dataset):
         else:
             if self.data_path.endswith("/"):
                 self.data_path = self.data_path[:-1]
-            self.files_names = list(glob.glob(f"{self.data_path}/**/*.pkl", recursive=True))
+            self.files_names = list(
+                glob.glob(f"{self.data_path}/**/*.pkl", recursive=True)
+            )
 
         log_object(f"{self.name}_files_names_before_shuffle", self.files_names)
         random.shuffle(self.files_names)
@@ -82,7 +85,11 @@ class PandasStaticDataProvider(torch.utils.data.Dataset):
                 continue
 
             self.successfully_loaded_files += 1
-            log_value(f"load_df_all_files_{self.name}", self.successfully_loaded_files, self.successfully_loaded_files)
+            log_value(
+                f"load_df_all_files_{self.name}",
+                self.successfully_loaded_files,
+                self.successfully_loaded_files,
+            )
             if self.p_sample:
                 load_df = load_df.sample(frac=self.p_sample)
             self.all_data.extend(self.process_df(load_df))
@@ -96,6 +103,28 @@ class PandasStaticSubgoalDataProvider(PandasStaticDataProvider):
     @staticmethod
     def process_df(df: pd.DataFrame):
         return subgoal_process_df(df)
+
+
+class PandasStaticSubgoalAllDistancesDataProvider(PandasStaticDataProvider):
+    def __init__(
+        self,
+        data_path: str,
+        files_batch_size: int = None,
+        p_sample: Optional[float] = None,
+        eval_num_samples: Optional[int] = None,
+        name: str = "default",
+        range_of_k: Optional[List[int]] = None,
+    ) -> None:
+        super().__init__(data_path, files_batch_size, p_sample, eval_num_samples, name)
+        if range_of_k is None:
+            range_of_k = [1]
+        self.range_of_k = range_of_k
+        assert (
+            min(self.range_of_k) >= 1 and max(self.range_of_k) <= 9
+        ), "Min k should be >= 1 and Max k should be <= 9"
+
+    def process_df(self, df: pd.DataFrame) -> List[Dict[str, List[int]]]:
+        return subgoal_all_k_process_df(df, self.range_of_k)
 
 
 class PandasStaticPolicyDataProvider(PandasStaticDataProvider):
