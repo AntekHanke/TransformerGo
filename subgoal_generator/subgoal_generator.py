62a43a0ffe28a35f8f057b5628c3861b7fd2e059
gracjan <gracjan.goral.b@gmail.com>
2023-03-08 12:40:12 +0100

    Adding class AdaChessSubgoalGenerator, which handles generating subgoals for given k

--- subgoal_generator/subgoal_generator.py
+++ subgoal_generator/subgoal_generator.py
@@ -11,7 +11,7 @@ from data_structures.data_structures import ImmutableBoard
 
 class ChessSubgoalGenerator:
     def generate_subgoals(
-        self, immutable_board: ImmutableBoard, n_subgoals: int, **subgoal_generation_kwargs
+            self, immutable_board: ImmutableBoard, n_subgoals: int, **subgoal_generation_kwargs
     ) -> List[ImmutableBoard]:
         raise NotImplementedError
 
@@ -24,7 +24,7 @@ class BasicChessSubgoalGenerator(ChessSubgoalGenerator):
             self.model = checkpoint_path_or_model
 
     def generate_subgoals(
-        self, input_board: ImmutableBoard, time_info: bool = False, **subgoal_generation_kwargs
+            self, input_board: ImmutableBoard, time_info: bool = False, **subgoal_generation_kwargs
     ) -> List[ImmutableBoard]:
         encoded_board = ChessTokenizer.encode_immutable_board(input_board) + [ChessTokenizer.vocab_to_tokens["<SEP>"]]
         input_tensor = torch.IntTensor([encoded_board]).to(self.model.device)
@@ -36,5 +36,33 @@ class BasicChessSubgoalGenerator(ChessSubgoalGenerator):
         for sequence in outputs:
             subgoals.append(ChessTokenizer.decode_board(sequence))
 
+        # subgoals = list({subgoal for subgoal in subgoals if subgoal.board != input_board.board})
+        return subgoals
+
+
+class AdaChessSubgoalGenerator(ChessSubgoalGenerator):
+    def __init__(self, checkpoint_path_or_model, subgoal_distance: int = 1) -> None
+        if isinstance(checkpoint_path_or_model, str):
+            self.model = BartForConditionalGeneration.from_pretrained(checkpoint_path_or_model)
+        else:
+            self.model = checkpoint_path_or_model
+        self.subgoal_distance = subgoal_distance
+        assert isinstance(self.subgoal_distance, int) and 10 > self.subgoal_distance > 0,\
+            "Subgoal distance must be an int in range [1, 10]"
+
+    def generate_subgoals(
+            self, input_board: ImmutableBoard, time_info: bool = False, **subgoal_generation_kwargs
+    ) -> List[ImmutableBoard]:
+        encoded_board = [self.subgoal_distance] + ChessTokenizer.encode_immutable_board(input_board) + [ChessTokenizer.vocab_to_tokens["<SEP>"]]
+        input_tensor = torch.IntTensor([encoded_board]).to(self.model.device)
+        time_start = time.time()
+        outputs = self.model.generate(input_tensor, max_new_tokens=80, **subgoal_generation_kwargs).tolist()
+        if time_info:
+            print(f"Subgoal generation time: {time.time() - time_start}")
+        subgoals = []
+        for sequence in outputs:
+            subgoals.append(ChessTokenizer.decode_board(sequence))
+
+        # TODO: Is this necessary?
         # subgoals = list({subgoal for subgoal in subgoals if subgoal.board != input_board.board})
         return subgoals
