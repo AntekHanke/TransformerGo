433a70c3e382f8857d94288572c675553d6e6169
Gosia <rog.malgosia@gmail.com>
2023-03-30 12:24:02 +0200

    Add ValueError

--- policy/cllp.py
+++ policy/cllp.py
@@ -84,7 +84,7 @@ class CLLP:
                 inputs_tokenized.append(
                     self.input_and_target_to_list_of_tokens(input_immutable_board, target_immutable_board)
                 )
-            except AssertionError:
+            except AssertionError or ValueError:
                 print(f"An error during tokenization occurred: {input_immutable_board}, {target_immutable_board}")
         moves_batch = self.generate_moves_batch_from_model(inputs_tokenized, num_beams, num_return_sequences)
         for query, paths in zip(queries_list, moves_batch):
