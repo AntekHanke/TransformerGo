011a797184447e11aad3b653b13570fc0107ebba
Gosia <rog.malgosia@gmail.com>
2023-04-14 12:04:27 +0200

    Game specifications

--- experiments/games_between_engines/game_between_subgoal_mcts_engines.py
+++ experiments/games_between_engines/game_between_subgoal_mcts_engines.py
@@ -4,22 +4,22 @@ import sys
 experiment_dir_path = os.path.dirname(os.path.abspath("__file__"))
 sys.path.append(experiment_dir_path)
 
-from experiments.games_between_engines.engine_specifications import stockfish_engine_params, subgoal_mcts_engine_params
+from experiments.games_between_engines.engine_specifications import subgoal_mcts_engine_params
 from mrunner.helpers.specification_helper import create_experiments_helper
 from configures.global_config import STOCKFISH_PATH
 
 GEN_LONG_K_3 = "/data_gg/out_models/medium_generator_from_scratch_with_all_subgoals/16_03_22/k_3/checkpoint-280500/"
 CLLP_PATH = "/data_gg/out_models/medium_cllp_from_scratch_k_form_1_to_9/14_03_23/checkpoint-144500/"
-log_dir: str = "/out_models/bot_logs/test"
 
 white_engine_params = subgoal_mcts_engine_params.copy()
 white_engine_params["generator_path"] = GEN_LONG_K_3
 white_engine_params["cllp_path"] = CLLP_PATH
-white_engine_params["max_mcts_passes"] = 30
+white_engine_params["max_mcts_passes"] = 15
 
-black_engine_params = stockfish_engine_params.copy()
-black_engine_params["stockfish_path"] = STOCKFISH_PATH
-black_engine_params["stockfish_depth"] = 8
+black_engine_params = subgoal_mcts_engine_params.copy()
+black_engine_params["generator_path"] = GEN_LONG_K_3
+black_engine_params["cllp_path"] = CLLP_PATH
+black_engine_params["max_mcts_passes"] = 15
 
 experiment_config = {
     "run.job_class": "@jobs.GameBetweenEngines",
@@ -39,14 +39,14 @@ params_grid = {
 }
 
 experiments_list = create_experiments_helper(
-    experiment_name=f"game-white-subgoal-mcts-30-black-stockfish-depth-8",
+    experiment_name=f"game-white-subgoal-mcts-?-black-subgoal-mcts-?",
     project_name="pmtest/subgoal-chess",
     base_config=experiment_config,
     params_grid=params_grid,
     script="python3 -m runner --mrunner",
     exclude=["data", ".pytest_cache", "out", ".git"],
     python_path="",
-    tags=["mcts", "random start", "game", "self-play", "stockfish"],
+    tags=["mcts", "game", "self-play", "mcts"],
     with_neptune=True,
     env={},
 )
