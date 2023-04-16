a24d70d49de5035bb5d2b30ce5fa1457a0f4ad0b
Antoni <antekhanke@gmail.com>
2023-04-17 00:21:39 +0200

    .py for ultra-small-model for go

--- experiments/train/policy/go_ultra_small_policy_from_scratch.py
+++ experiments/train/policy/go_ultra_small_policy_from_scratch.py
@@ -4,21 +4,25 @@ import sys
 experiment_dir_path = os.path.dirname(os.path.abspath("__file__"))
 sys.path.append(experiment_dir_path)
 
-from experiments.data_loading_params import policy_data_loading_header, go_ultra_small_data
+from experiments.data_loading_params import policy_data_loading_headerGo, go_ultra_small_data
 from experiments.models_params import common_train_params, go_ultra_small_model
 from mrunner.helpers.specification_helper import create_experiments_helper
 
 TRAIN_TYPE = "from_scratch"
 HISTORY = ""
-OUT_DIR = "/out_models/ultra_small_policy_from_scratch"
+OUT_DIR = "/plgantekhanke/models/ultra_small_policy_from_scratch"
 
 experiment_config = dict(
     **common_train_params[TRAIN_TYPE],
-    **policy_data_loading_header[TRAIN_TYPE + HISTORY],
+    **policy_data_loading_headerGo[TRAIN_TYPE + HISTORY],
     **go_ultra_small_data[TRAIN_TYPE],
     **go_ultra_small_model,
+
 )
 experiment_config["TrainModelFromScratch.out_dir"] = OUT_DIR
+
+experiment_config["TrainModelFromScratch.path_to_training_data"] = "/plgantekhanke/tokenized_data/test/"
+experiment_config["TrainModelFromScratch.path_to_eval_data"] = "/plgantekhanke/tokenized_data/test/"
 
 params_grid = {
     "idx": [0],
@@ -30,9 +34,9 @@ experiments_list = create_experiments_helper(
     base_config=experiment_config,
     params_grid=params_grid,
     script="python3 -m runner --mrunner",
-    exclude=["data", ".pytest_cache", "out", ".git"],
+    exclude=["data", ".pytest_cache", "out", ".git", "exclude"],
     python_path="",
-    tags=["train", "small", "policy", "from_scratch"],
+    tags=["train", "small", "policy", "from_scratch", "go", "cluster", "transformer", "testing"],
     with_neptune=True,
     env={},
 )
