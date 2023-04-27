e890505f6442508df4843c9d69622bec301afc5c
Antoni <antekhanke@gmail.com>
2023-04-27 21:35:24 +0200

    Python script to generate naive value data

--- experiments/data_generation/Go/ValueDataGeneration.py
+++ experiments/data_generation/Go/ValueDataGeneration.py
@@ -2,16 +2,16 @@ from mrunner.helpers.specification_helper import create_experiments_helper
 
 base_config = {
     # job's parameters
-    "run.job_class": "@jobs.GoTokenizedPolicyGeneratorAlwaysBlack",
-    "GoTokenizedPolicyGeneratorAlwaysBlack.GoTokenizedDataGenerator": "@data.GoSimpleGamesDataGeneratorTokenizedAlwaysBlack",
+    "run.job_class": "@jobs.GoTokenizedValueGenerator",
+    "GoTokenizedValueGenerator.GoTokenizedDataGenerator": "@data.GoValueTokenized",
     # class's parametres (ChessSubgoalGamesDataGenerator)
-    "GoSimpleGamesDataGeneratorTokenizedAlwaysBlack.sgf_files": "/plgantekhanke/raw_data/sgfs/test.txt",
-    "GoSimpleGamesDataGeneratorTokenizedAlwaysBlack.save_data_every_n_games": 29990,
-    "GoSimpleGamesDataGeneratorTokenizedAlwaysBlack.p_sample": 1.0,
-    "GoSimpleGamesDataGeneratorTokenizedAlwaysBlack.max_games": 19950,
-    "GoSimpleGamesDataGeneratorTokenizedAlwaysBlack.train_eval_split": 0.0,
-    "GoSimpleGamesDataGeneratorTokenizedAlwaysBlack.save_path_to_eval_set": "/plgantekhanke/tokenized_data/test/",
-    "GoSimpleGamesDataGeneratorTokenizedAlwaysBlack.save_path_to_train_set": "/plgantekhanke/tokenized_data/test/",
+    "GoValueTokenized.sgf_files": "/plgantekhanke/raw_data/sgfs/test.txt",
+    "GoValueTokenized.save_data_every_n_games": 29990,
+    "GoValueTokenized.p_sample": 1.0,
+    "GoValueTokenized.max_games": 15000,
+    "GoValueTokenized.train_eval_split": 0.0,
+    "GoValueTokenized.save_path_to_eval_set": "/plgantekhanke/tokenized_value_data/test/",
+    "GoValueTokenized.save_path_to_train_set": "/plgantekhanke/tokenized_value_data/test/",
     # class's parameters (ELOFilter)
 
     "use_neptune": True,
@@ -23,14 +23,14 @@ params_grid = {
 }
 
 experiments_list = create_experiments_helper(
-    experiment_name=f"generating-tokenized-go-dataset-black",
+    experiment_name=f"generating-tokenized-go-dataset-black-value",
     project_name="pmtest/subgoal-chess",
     base_config=base_config,
     params_grid=params_grid,
     script="python3 -m runner --mrunner",
     exclude=['sgf', 'val', 'exclude'], #niepotrzebne pliki do clustra
     python_path="",
-    tags=["dataset", "generate", "tokenized", "go", "test"],
+    tags=["dataset", "generate", "tokenized", "go", "test", "value"],
     with_neptune=True,
     env={},
 )
