eb7bb13c5a33db6abb0b12d16f6b932a1f4cb527
Antoni <antekhanke@gmail.com>
2023-04-27 21:42:44 +0200

    Go tokenized value job

--- jobs/local_jobs_antek/go_data_generator_tokenized_value.py
+++ jobs/local_jobs_antek/go_data_generator_tokenized_value.py
@@ -1,10 +1,10 @@
 from typing import Type
 
 from jobs.core import Job
-from data_processing.go_data_generator import GoSimpleGamesDataGeneratorTokenizedAlwaysBlack
+from data_processing.go_data_generator import GoValueTokenized
 
-class GoTokenizedPolicyGeneratorAlwaysBlack(Job):
-    def __init__(self, GoTokenizedDataGenerator: Type[GoSimpleGamesDataGeneratorTokenizedAlwaysBlack]):
+class GoTokenizedValueGenerator(Job):
+    def __init__(self, GoTokenizedDataGenerator: Type[GoValueTokenized]):
         self.GoTokenizedDataGenerator = GoTokenizedDataGenerator()
 
     def execute(self):
