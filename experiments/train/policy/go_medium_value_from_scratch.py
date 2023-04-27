1cb903c12f6466f87ab009f37f23334d535d5e71
Antoni <antekhanke@gmail.com>
2023-04-27 21:37:36 +0200

    Value training runs

--- experiments/train/policy/go_medium_value_from_scratch.py
+++ experiments/train/policy/go_medium_value_from_scratch.py
@@ -5,24 +5,24 @@ experiment_dir_path = os.path.dirname(os.path.abspath("__file__"))
 sys.path.append(experiment_dir_path)
 
 from experiments.data_loading_params import policy_data_loading_headerGo, go_medium_data
-from experiments.models_params import common_train_params, go_medium_model
+from experiments.models_params import common_train_params, go_medium_model_value
 from mrunner.helpers.specification_helper import create_experiments_helper
 
 TRAIN_TYPE = "from_scratch"
 HISTORY = ""
-OUT_DIR = "/plgantekhanke/models/medium_policy_from_scratch_full"
+OUT_DIR = "/plgantekhanke/models/medium_value_from_scratch_full"
 
 experiment_config = dict(
     **common_train_params[TRAIN_TYPE],
     **policy_data_loading_headerGo[TRAIN_TYPE + HISTORY],
     **go_medium_data[TRAIN_TYPE],
-    **go_medium_model,
+    **go_medium_model_value,
 
 )
 experiment_config["TrainModelFromScratch.out_dir"] = OUT_DIR
 
-experiment_config["TrainModelFromScratch.path_to_training_data"] = "/plgantekhanke/tokenized_data/train/"
-experiment_config["TrainModelFromScratch.path_to_eval_data"] = "/plgantekhanke/tokenized_data/test/"
+experiment_config["TrainModelFromScratch.path_to_training_data"] = "/plgantekhanke/tokenized_value_data/train/"
+experiment_config["TrainModelFromScratch.path_to_eval_data"] = "/plgantekhanke/tokenized_value_data/test/"
 
 params_grid = {
     "idx": [0],
@@ -36,7 +36,7 @@ experiments_list = create_experiments_helper(
     script="python3 -m runner --mrunner",
     exclude=["data", ".pytest_cache", "out", ".git", "exclude"],
     python_path="",
-    tags=["train", "medium", "policy", "from_scratch", "go", "cluster", "transformer", "full"],
+    tags=["train", "medium", "value", "from_scratch", "go", "cluster", "transformer", "full"],
     with_neptune=True,
     env={},
 )
